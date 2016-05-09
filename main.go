9e594d78de1d2d1d6c1281f79a813b665b932c84
Stéphane Klein <stephaneklein221@gmail.com>
2016-05-09 22:51:27 +0200

    Use q verb for quoted strings

--- main.go
+++ main.go
@@ -21,15 +21,15 @@ func (c Cache) AddHandler(h Handler) {
 	version, err := h.Version()
 
 	if err != nil {
-		log.Printf("Cannot add handler '%s': unable to get underlying version (package manager might not be available)", h.Name())
+		log.Printf("Cannot add handler %q: unable to get underlying version (package manager might not be available)", h.Name())
 	} else {
-		log.Printf("Adding handler '%s' (version: %s)", h.Name(), version)
+		log.Printf("Adding handler %q (version: %s)", h.Name(), version)
 		c.handlers = append(c.handlers, h)
 	}
 }
 
 func (c Cache) Process(requestDir string) (solvedTar string, err error) {
-	log.Printf("Processing directory '%s'", requestDir)
+	log.Printf("Processing directory %q", requestDir)
 
 	files, err := ioutil.ReadDir(requestDir)
 
@@ -48,14 +48,14 @@ func (c Cache) Process(requestDir string) (solvedTar string, err error) {
 
 		if applicable {
 			hasHandler = true
-			log.Printf("Applying handler '%s'", h.Name())
+			log.Printf("Applying handler %q", h.Name())
 		} else {
-			log.Printf("Handler '%s' not applicable to request dir '%s'", h.Name(), requestDir)
+			log.Printf("Handler %q not applicable to request dir %q", h.Name(), requestDir)
 		}
 	}
 
 	if !hasHandler {
-		return "", fmt.Errorf("No applicable handler for request dir '%s'", requestDir)
+		return "", fmt.Errorf("No applicable handler for request dir %q", requestDir)
 	}
 
 	return "test", nil
